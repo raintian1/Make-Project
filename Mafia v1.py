72ac855cbbb640b60964addc892cb1a6ee1b25b1
raintian1 <61839499+raintian1@users.noreply.github.com>
2020-04-29 15:53:52 -0400

    Update target check
    target check is now in a new function instead of being under the Godfather/Mafioso functions. Easier to edit that way

--- Mafia v1.py
+++ Mafia v1.py
@@ -1,7 +1,10 @@
+#MAFIA PROJECT
 import random, time
 
 print('\033[1m' + '''Welcome to Mafia! Input the number of players:''' + '\033[0m')
 
+# Change this to dictionary (or dictionary containing a dictionary, ie: when you look up John, it will display his role, dead/alive, framed status etc.)
+# Or create a class variable for role? for XXX in class: if status == 'alive'...
 roles = ['Sheriff', 'Doctor', 'Mayor', 'Escort', 'Amnesiac', 'Godfather', 'Framer', 'Jester', 'Jailor', 'Mafioso', 'Vigilante', 'Executioner']
 playerList = []
 
@@ -31,6 +34,7 @@ def inputPlayers():
         players = input()
         playerList.append(players)
 
+# Maybe use dictionary here (call on integer values instead of looking up a word)
 def assignRoles():
     global merged_list
     random_role = random.sample(roles_transformed, k = playerNumber)
@@ -48,44 +52,97 @@ def nightTime():
     print('\033[1m' + 'Night Time...' + '\033[0m')
 
 def godfatherNightCycle():
-    global Godfather_alive
-    global Godfather_target
+    global godfather_alive
+    global godfather_target
 
+    print()
+    print('Now entering Godfather action phase...')
     print('Is the Godfather still alive? (y/n)')
-    Godfather_alive = ''
-    while Godfather_alive != 'y' or Godfather_alive != 'n':
-        Godfather_alive = str(input())
-        if Godfather_alive == 'y':
+    godfather_alive = ''
+    godfather_target = ''
+
+    while godfather_alive != 'y' or godfather_alive != 'n':
+        godfather_alive = str(input())
+        if godfather_alive == 'y':
             print('Who will the Godfather appoint to kill?')
-            Godfather_target = ''
-            while Godfather_target not in playerList:
-                Godfather_target = str(input())
-                if Godfather_target not in playerList:
-                    print('Player not found, try again.')
-            return Godfather_target
-        elif Godfather_alive == 'n':
-            mafiosoNightCycle()
+            godfather_target = ''
+            while godfather_target not in playerList:
+                godfather_target = str(input())
+                if godfather_target not in playerList:
+                    print('Player not found, try again')
+                elif godfather_target == merged_list[int(godfather_target)-1]:
+                    print('Cannot target self.')
+            return godfather_target
+        elif godfather_alive == 'n':
+            print('The Godfather is dead')
+            return godfather_alive
         else:
             print('Select y/n, try again')
-    return Godfather_alive
+    return godfather_alive
 
-# Fix from here down!!!
 def mafiosoNightCycle():
-    print('Who will the Mafioso shoot?')
-    Mafioso_target = str(input())
+    global mafioso_alive
+    global mafioso_target
 
-    if Godfather_target != Mafioso_target:
-        Mafia_side_target = Godfather_target
-        print('The Mafioso must comply with the Godfather, ' + Godfather_target + ''' is tonight's target.''')
-    else:
-        print('The Mafia came to an agreement, ' + Godfather_target + ''' is tonight's target.''')
+    print()
+    print('Now entering Mafioso action phase...')
+    print('Is the Mafioso still alive? (y/n)')
+    mafioso_alive = ''
+    mafioso_target = ''
+
+    while mafioso_alive != 'y' or mafioso_alive != 'n':
+        mafioso_alive = str(input())
+        if mafioso_alive == 'y':
+            print('Who will the Mafioso kill?')
+            mafioso_target = ''
+            while mafioso_target not in playerList:
+                mafioso_target = str(input())
+                if mafioso_target not in playerList:
+                    print('Player not found, try again')
+            return mafioso_target
+        elif mafioso_alive == 'n':
+            print('The Mafioso is dead')
+            return mafioso_alive
+        else:
+            print('Select y/n, try again')
+    return mafioso_alive
 
 def framerNightCycle():
+    global framer_alive
+    global framer_target
+
     print()
-    print('Who will the framer choose to frame?')
-    Framer_target = str(input())
-    roles[Framer_target] == 'framed'
-    print(Framer_target + ', the ' + roles[Framer_target] + ' , will appear guilty under investigation tonight.')
+    print('Now entering Framer action phase...')
+    print('Is the Framer still alive? (y/n)')
+    framer_alive = ''
+    while framer_alive != 'y' or framer_alive != 'n':
+        framer_alive = str(input())
+        if framer_alive == 'y':
+            print('Who will the Framer frame?')
+            framer_target = ''
+            while framer_target not in playerList:
+                framer_target = str(input())
+                if framer_target not in playerList:
+                    print('Player not found, try again')
+            return framer_target
+        elif framer_alive == 'n':
+            print('The Framer is dead')
+            return framer_alive
+        else:
+            print('Select y/n, try again')
+    return framer_alive
+
+def mafiaTargetCheck():
+    print()
+    time.sleep(1)
+    if godfather_alive == 'n':
+        print('With no Godfather, ' + mafioso_target + ''' becomes tonight's target.''')
+    elif mafioso_alive == 'n':
+        print('With no Mafioso, ' + godfather_target + ''' becomes tonight's target.''')
+    elif godfather_target != mafioso_target:
+        print('The Mafioso must comply with the Godfather, ' + godfather_target + ''' is tonight's target.''')
+    else:
+        print('The Mafia came to an agreement, ' + godfather_target + ''' is tonight's target.''')
 
 transfromList()
 inputPlayers()
@@ -93,3 +150,5 @@ assignRoles()
 nightTime()
 godfatherNightCycle()
 mafiosoNightCycle()
+framerNightCycle()
+mafiaTargetCheck()
